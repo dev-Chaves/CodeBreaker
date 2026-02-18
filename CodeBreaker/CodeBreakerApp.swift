be1ada8a8cda287be1f83bd5fc07ae847bfa5c35
Joao Vitor <chavesprogrammer@gmail.com>
2026-02-18 07:25:29 -0300

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by João Vitor Chaves on 18/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
