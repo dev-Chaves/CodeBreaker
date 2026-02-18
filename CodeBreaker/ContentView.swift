a933433a44e4531317d0e368b6c8cb3f40725678
Joao Vitor <chavesprogrammer@gmail.com>
2026-02-18 07:44:55 -0300

    commit inicial

--- CodeBreaker/ContentView.swift
+++ CodeBreaker/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var helloWord: String = "Hello, World!"
+    
+    var count: Int = 0
+    
+    var test: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        VStack(alignment: .center, spacing: 64, content: greetings)
         .padding()
+        
+        something
+    }
+    
+    var something: some View {
+        HStack{
+            if(test == true){
+                Text("Hello!")
+            }else{
+                Text("Not True")
+            }
+            
+            
+        }
+    }
+    
+    func counting(i: Int) -> Int{
+        return i + 1
+    
+    }
+    
+    @ViewBuilder
+    func greetings() -> some View {
+        Image(systemName: "globe").font(.largeTitle)
+        Text("Hello down there!").font(.largeTitle)
+        Circle().padding().frame(width: 100, height: 100)
+    }
+    
+    func bolinhas () -> some View {
+        HStack{
+            Circle()
+            Circle()
+            Circle()
+            Circle()
+        }
     }
 }
 
